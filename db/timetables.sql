35486c954badd9806dc91f50802185254e050c53
KarmelitaK <Kai.Brunner@mng.ch>
2026-01-07 00:02:10 +0100

    Reworked_timetable.sql
    Foreign key in timetable instead of others, fixed cross_timetables

--- db/timetables.sql
+++ db/timetables.sql
@@ -14,55 +14,54 @@ DROP TABLE cross_timetable_teacher;
 DROP TABLE cross_timetable_room;
 
 -- We could also use INT for a couple of things and just number them through but at the end we'd prolly
--- have to make more tables. Also it mostly depends on how we recieve the info.
-
--- We'll have to check if I put the foreign keys in the right tables
-
--- Have to add n-n possibilities for tabled, otherwise should mostly be good -- Done That
+-- Times table maybe unneccesary cuz does not save anything, have to look into that, maybe just normally save it in timetable
+-- Problem with grade and absences in how we currently do it, we do not have the student, only works in app not website
 
 CREATE TABLE timetable (
     id INT AUTO_INCREMENT PRIMARY KEY
+    absence_id INT,
+    subject_id INT,
+    grade_id INT,
+    homework_id INT,
+    message_id INT,
+    times_id INT NOT NULL,
+    special_id INT,
+    FOREIGN KEY (absence_id) REFERENCES absence(id)
+    FOREIGN KEY (subject_id) REFERENCES subject(id)
+    FOREIGN KEY (grade_id) REFERENCES grade(id)
+    FOREIGN KEY (homework_id) REFERENCES homework(id)
+    FOREIGN KEY (message_id) REFERENCES message(id)
+    FOREIGN KEY (times_id) REFERENCES times(id)
+    FOREIGN KEY (special_id) REFERENCES special(id)
 );
 
 CREATE TABLE teacher (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     initials VARCHAR(10) NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (cross_timetable_id) REFERENCES cross_timetable_teacher(teacher_id)
 );
 
 CREATE TABLE room (
     id INT AUTO_INCREMENT PRIMARY KEY,
     num VARCHAR(10) NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (cross_timetable_id) REFERENCES cross_timetable_room(room_id)
 );
 
 CREATE TABLE absence (
     id INT AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(50) NOT NULL,
     status VARCHAR(50) NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
 );
 
 CREATE TABLE subject (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     shortened VARCHAR (10) NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
 );
 
 CREATE TABLE grade (
     id INT AUTO_INCREMENT PRIMARY KEY,
     mark FLOAT,
     weight FLOAT,
-    timetable_id INT,
-    subject_id INT,
-    FOREIGN KEY (subject_id) REFERENCES subject(id),
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
 );
 
 CREATE TABLE homework (
@@ -70,50 +69,47 @@ CREATE TABLE homework (
     txt VARCHAR(500),
     file BOOLEAN NOT NULL,
     title VARCHAR(100),
-    timetable_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
 );
 
 CREATE TABLE message (
     id INT AUTO_INCREMENT PRIMARY KEY,
     message VARCHAR(500),
-    timetable_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
 );
 
 CREATE TABLE class (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (cross_timetable_id) REFERENCES cross_timetable_class(timetable_id)
 );
 
 CREATE TABLE times (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    time INT NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
+    time INT NOT NULL
 );
 CREATE TABLE special (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(100) NOT NULL,
-    timetable_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
+    type VARCHAR(100) NOT NULL
 );
+
 --Crosstables
     
 CREATE TABLE cross_timetable_class (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    timetable_id INT,
+    class_id INT,
     FOREIGN KEY (timetable_id) REFERENCES timetable(id)
     FOREIGN KEY (class_id) REFERENCES class(id)
 );
 CREATE TABLE cross_timetable_teacher (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    timetable_id INT,
+    teacher_id INT,
     FOREIGN KEY (timetable_id) REFERENCES timetable(id)
     FOREIGN KEY (teacher_id) REFERENCES teacher(id)
 );
 CREATE TABLE cross_timetable_room (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    timetable_id INT,
+    room_id INT,
     FOREIGN KEY (timetable_id) REFERENCES timetable(id)
     FOREIGN KEY (room_id) REFERENCES room(id)
 );
