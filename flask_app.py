3661a248ad5aad6f70fefbec4baeb674c2a623be
JuyCeee <juyceee@gmail.com>
2026-01-07 07:49:57 +0100

    d

--- flask_app.py
+++ flask_app.py
@@ -148,7 +148,7 @@ def timetable():
     # GET
     if request.method == "GET":
         todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
-        return render_template("main_page.html", todos=todos)
+        return render_template("timetable.html", todos=todos)
 
     # POST
     content = request.form["contents"]
