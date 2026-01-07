93a7d35f14297bcc7a8a3a653b2412939d22db04
KarmelitaK <Kai.Brunner@mng.ch>
2026-01-07 23:30:44 +0100

    Fixed timetables.sql
    No errors anymore as far as I can tell (dunno bout the tempdata, the rest workos now tho)

--- db/timetables.sql
+++ db/timetables.sql
@@ -17,101 +17,71 @@ DROP TABLE cross_timetable_room;
 -- Times table maybe unneccesary cuz does not save anything, have to look into that, maybe just normally save it in timetable
 -- Problem with grade and absences in how we currently do it, we do not have the student, only works in app not website
 
-CREATE TABLE timetable (
-    id INT AUTO_INCREMENT PRIMARY KEY
-    absence_id INT,
-    subject_id INT,
-    grade_id INT,
-    homework_id INT,
-    message_id INT,
-    start_time INT NOT NULL,
-    end_time INT NOT NULL,
-    special_id INT,
-    FOREIGN KEY (absence_id) REFERENCES absence(id)
-    FOREIGN KEY (subject_id) REFERENCES subject(id)
-    FOREIGN KEY (grade_id) REFERENCES grade(id)
-    FOREIGN KEY (homework_id) REFERENCES homework(id)
-    FOREIGN KEY (message_id) REFERENCES message(id)
-    FOREIGN KEY (times_id) REFERENCES times(id)
-    FOREIGN KEY (special_id) REFERENCES special(id)
-);
-
-CREATE TABLE teacher (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    initials VARCHAR(10) NOT NULL,
-);
-
-CREATE TABLE room (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    num VARCHAR(10) NOT NULL,
-);
-
-CREATE TABLE absence (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-);
-
 CREATE TABLE subject (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    shortened VARCHAR (10) NOT NULL,
-);
-
-CREATE TABLE grade (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    mark FLOAT,
-    weight FLOAT,
+    shortened VARCHAR (10) NOT NULL
 );
 
 CREATE TABLE homework (
     id INT AUTO_INCREMENT PRIMARY KEY,
     txt VARCHAR(500),
     file BOOLEAN NOT NULL,
-    title VARCHAR(100),
+    title VARCHAR(100)
 );
 
 CREATE TABLE message (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    message VARCHAR(500),
+    message VARCHAR(500)
 );
 
-CREATE TABLE class (
+CREATE TABLE class_ (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(10) NOT NULL,
+    name VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE times (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    time INT NOT NULL
-);
 CREATE TABLE special (
     id INT AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(100) NOT NULL
 );
 
---Crosstables
-    
+CREATE TABLE timetable (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    subject_id INT,
+    homework_id INT,
+    message_id INT,
+    start_time INT NOT NULL,
+    end_time INT NOT NULL,
+    special_id INT,
+    FOREIGN KEY (subject_id) REFERENCES subject(id),
+    FOREIGN KEY (homework_id) REFERENCES homework(id),
+    FOREIGN KEY (message_id) REFERENCES message(id),
+    FOREIGN KEY (special_id) REFERENCES special(id)
+);
+
+--cross_timetables
+
 CREATE TABLE cross_timetable_class (
     id INT AUTO_INCREMENT PRIMARY KEY,
     timetable_id INT,
     class_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
-    FOREIGN KEY (class_id) REFERENCES class(id)
+    FOREIGN KEY (timetable_id) REFERENCES timetable(id),
+    FOREIGN KEY (class_id) REFERENCES class_(id)
 );
+
 CREATE TABLE cross_timetable_teacher (
     id INT AUTO_INCREMENT PRIMARY KEY,
     timetable_id INT,
     teacher_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
+    FOREIGN KEY (timetable_id) REFERENCES timetable(id),
     FOREIGN KEY (teacher_id) REFERENCES teacher(id)
 );
+
 CREATE TABLE cross_timetable_room (
     id INT AUTO_INCREMENT PRIMARY KEY,
     timetable_id INT,
     room_id INT,
-    FOREIGN KEY (timetable_id) REFERENCES timetable(id)
+    FOREIGN KEY (timetable_id) REFERENCES timetable(id),
     FOREIGN KEY (room_id) REFERENCES room(id)
 );
 
