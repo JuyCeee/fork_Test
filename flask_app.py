f12113f5fa76722cd9dc5384b658cac75c61d81b
JuyCeee <juyceee@gmail.com>
2026-01-08 10:42:50 +0100

    f

--- flask_app.py
+++ flask_app.py
@@ -165,6 +165,7 @@ def timetable():
     
     # Optional: Zeitstempel (ms) in lesbare Objekte umwandeln, falls nötig
     # Hier übergeben wir die Rohdaten an das Template
+    timetable = db_read("SELECT username FROM users ORDER BY username", ())
     return render_template("timetable.html", timetable=timetable)
 
 @app.route("/", methods=["GET", "POST"])
