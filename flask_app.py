c6f499dd8384cfe9fc207f36b8de764819166469
JuyCeee <juyceee@gmail.com>
2026-01-09 16:15:21 +0100

    d

--- flask_app.py
+++ flask_app.py
@@ -143,8 +143,8 @@ def overview():
         s.name AS subject_name, 
         s.shortened AS subject_short,
         h.txt AS homework_text,
-        h.id
-        t.exam
+        h.id,
+        t.exam,
         m.message AS lesson_message
     FROM timetable t
     LEFT JOIN subject s ON t.subject_id = s.id
